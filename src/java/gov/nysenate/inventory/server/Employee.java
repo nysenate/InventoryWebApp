a85d13135f3f0a9f7951438e35882348baa4fd07
heitner <heitner@nysenate.gov>
2013-05-14 11:05:40 -0400

    Modifications made to Employee which allow for Employee Name to be returned in different orders, default order was also changed.

--- src/java/gov/nysenate/inventory/server/Employee.java
+++ src/java/gov/nysenate/inventory/server/Employee.java
@@ -20,6 +20,15 @@ public class Employee {
   private transient String namidinit = null;
   private transient String nasuffix = null;
   private String naemployee = null;
+  public final int LAST_SUFFIX_FIRST_MI = -1000;
+  public final int LAST_FIRST_MI = -1001;
+  public final int LAST_SUFFIX_FIRST = -1002;
+  public final int FIRST_MI_LAST_SUFFIX = -1003;
+  public final int FIRST_MI_LAST = -1004;
+  public final int FIRST_LAST = -1005;
+  public final int LAST_FIRST = -1006;
+  public final int FIRST_LAST_SUFFIX = -1007;
+  private int employeeNameOrder = FIRST_MI_LAST_SUFFIX; 
   
   public Employee () {
   
@@ -27,6 +36,11 @@ public class Employee {
 
   public Employee (int nuxrefem, String nafirst, String nalast) {
         this(nuxrefem, nafirst, nalast, null, null);
+        formatEmployeeName(this.FIRST_MI_LAST_SUFFIX);        
+  }
+  public Employee (int nuxrefem, String nafirst, String nalast,int employeeNameOrder) {
+        this(nuxrefem, nafirst, nalast, null, null);
+        formatEmployeeName(this.FIRST_MI_LAST_SUFFIX);        
   }
   
   public Employee (int nuxrefem, String nafirst, String nalast,String namidinit,String nasuffix) {
@@ -35,44 +49,107 @@ public class Employee {
        this.nalast = nalast;
        this.namidinit = namidinit;
        this.nasuffix = nasuffix;
-       StringBuilder s = new StringBuilder();
-       s.append(nalast);
-       if (nasuffix != null && nasuffix.trim().length()>0) {
-            s.append(" ");
-            s.append(nasuffix);
-       }
-       s.append(", ");
-       s.append(nafirst);
-       if (namidinit != null && namidinit.trim().length()>0) {
-            s.append(" ");
-            s.append(namidinit);
-       }
-       this.naemployee = s.toString();
+       formatEmployeeName(this.FIRST_MI_LAST_SUFFIX);
   }
-  
-  public void setEmployeeData(int nuxrefem, String nafirst, String nalast) {
-       setEmployeeData(nuxrefem, nafirst, nalast, null, null);
-  }
-  
-  public void setEmployeeData(int nuxrefem, String nafirst, String nalast,String namidinit,String nasuffix) {
+
+  public Employee (int nuxrefem, String nafirst, String nalast,String namidinit,String nasuffix,int employeeNameOrder) {
        this.nuxrefem = nuxrefem;
        this.nafirst = nafirst;
        this.nalast = nalast;
        this.namidinit = namidinit;
        this.nasuffix = nasuffix;
-       StringBuilder s = new StringBuilder();
-       s.append(nalast);
-       if (nasuffix != null && nasuffix.trim().length()>0) {
-            s.append(" ");
-            s.append(nasuffix);
+       formatEmployeeName(employeeNameOrder);
+  }
+  
+  
+  public void setEmployeeData(int nuxrefem, String nafirst, String nalast) {
+       setEmployeeData(nuxrefem, nafirst, nalast, null, null, this.FIRST_MI_LAST_SUFFIX);
+  }
+  
+  public void setEmployeeData(int nuxrefem, String nafirst, String nalast, int employeeNameOrder) {
+       setEmployeeData(nuxrefem, nafirst, nalast, null, null, employeeNameOrder);
+  }
+
+  public void setEmployeeData(int nuxrefem, String nafirst, String nalast,String namidinit,String nasuffix) {
+      setEmployeeData(nuxrefem, nafirst, nalast,namidinit,nasuffix, FIRST_MI_LAST_SUFFIX);
+  }
+
+  public void setEmployeeData(int nuxrefem, String nafirst, String nalast,String namidinit,String nasuffix, int employeeNameOrder) {
+       this.nuxrefem = nuxrefem;
+       this.nafirst = nafirst;
+       this.nalast = nalast;
+       this.namidinit = namidinit;
+       this.nasuffix = nasuffix;
+       formatEmployeeName(employeeNameOrder);
+  }
+   
+  private void formatEmployeeName(int employeeNameOrder) {
+      StringBuilder s = new StringBuilder();     
+      switch (employeeNameOrder) {
+          case FIRST_LAST:
+          case FIRST_LAST_SUFFIX:
+          case FIRST_MI_LAST:
+          case FIRST_MI_LAST_SUFFIX:
+          case LAST_FIRST:
+          case LAST_FIRST_MI:
+          case LAST_SUFFIX_FIRST:
+          case LAST_SUFFIX_FIRST_MI:
+              break;
+          default:
+              employeeNameOrder = FIRST_MI_LAST_SUFFIX;
+      }
+       
+      this.employeeNameOrder = employeeNameOrder;
+
+      if (employeeNameOrder==LAST_SUFFIX_FIRST_MI||employeeNameOrder==LAST_FIRST_MI||employeeNameOrder==LAST_SUFFIX_FIRST||employeeNameOrder==LAST_FIRST) {
+            s.append(nalast);
+            if (employeeNameOrder==LAST_SUFFIX_FIRST_MI||employeeNameOrder==LAST_SUFFIX_FIRST) {
+                if (nasuffix != null && nasuffix.trim().length()>0) {
+                        s.append(" ");
+                        s.append(nasuffix);
+                }
+            }
+            s.append(", ");
+            s.append(nafirst);
+            if (employeeNameOrder==LAST_SUFFIX_FIRST_MI||employeeNameOrder==LAST_FIRST_MI) {
+
+                if (namidinit != null && namidinit.trim().length()>0) {
+                      s.append(" ");
+                      s.append(namidinit);
+                }
+            }
        }
-       s.append(", ");
-       s.append(nafirst);
-       if (namidinit != null && namidinit.trim().length()>0) {
+      else  {
+            s.append(nafirst);
+            if (employeeNameOrder==FIRST_MI_LAST_SUFFIX||employeeNameOrder==FIRST_MI_LAST) {
+
+                if (namidinit != null && namidinit.trim().length()>0) {
+                      s.append(" ");
+                      s.append(namidinit);
+                }
+            }
             s.append(" ");
-            s.append(namidinit);
-       }
-       this.naemployee = s.toString();
+            s.append(nalast);
+            if (employeeNameOrder==FIRST_MI_LAST_SUFFIX||employeeNameOrder==FIRST_LAST_SUFFIX) {
+                if (nasuffix != null && nasuffix.trim().length()>0) {
+                        s.append(" ");
+                        s.append(nasuffix);
+                }
+            }
+       } 
+       this.naemployee = s.toString();      
+  }
+  
+  public int getEmployeeNameOrder() {
+      return this.employeeNameOrder;
+  }
+
+  public void setEmployeeNameOrder(int employeeNameOrder) {
+      formatEmployeeName(employeeNameOrder); 
+      // formatEmployeeName is meant to format the name, but it also sets the
+      // employeeNameOrder value, it is kept private since the name would be to
+      // to confusing. setEmployeeNameOrder is more descriptive on what the developer
+      // wants to do, and in the process the Employee Name has to get formated again.
   }
   
   public String getEmployeeName() {
