62a5cd99cee4d2369e5ee00011d08eadfd7419d4
Ken Zalewski <zalewski.senate@gmail.com>
2013-06-07 12:03:09 -0400

    Fixed spelling error.  Reformatted code.  Removed DOS line endings.

--- src/main/java/gov/nysenate/inventory/server/Search.java
+++ src/main/java/gov/nysenate/inventory/server/Search.java
@@ -1,11 +1,5 @@
 package gov.nysenate.inventory.server;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
@@ -20,8 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  * @author Patil
  */
 @WebServlet(name = "Search", urlPatterns = {"/Search"})
-public class Search extends HttpServlet {
-
+public class Search extends HttpServlet
+{
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -33,7 +27,8 @@ public class Search extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+                   throws ServletException, IOException
+    {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
@@ -46,38 +41,36 @@ public class Search extends HttpServlet {
             out.println("<h1>Servlet Search at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");  */
-           String barcode_num=request.getParameter("barcode_num");
-            System.out.println("Serch Servlet  barcode_num "+barcode_num);
-          //  out.println("Barcode # "+barcode_num);
+            String barcode_num = request.getParameter("barcode_num");
+            System.out.println("Search Servlet  barcode_num "+barcode_num);
+            //  out.println("Barcode # "+barcode_num);
             
             //int barcode=Integer.valueOf(barcode_num);
-             System.out.println("Serch Servlet  barcode "+barcode_num);
-             DbConnect db=new DbConnect();
-            String details= db.getDetails(barcode_num);
+            System.out.println("Search Servlet  barcode "+barcode_num);
+            DbConnect db = new DbConnect();
+            String details = db.getDetails(barcode_num);
             
-          if (details.equals("no")) {
+            if (details.equals("no")) {
               
-             out.println("Does not exist in system");
-          } else{
-            String model[]= details.split("\\|");
-            System.out.println("DETAILS:"+details+" MODEL LENGTH:"+model.length);
-            // out.println(" Model   :  "+model[0]+"\n Location :  "+model[1]+"\n Manufacturer : "+model[2]+"\n Signed By  :    "+model[3]);
-           //V_NUSENATE,V_NUXREFSN,V_NUSERIAL,V_DTISSUE,V_CDLOCATTO,V_CDLOCTYPETO,V_CDCATEGORY,V_DECOMMODITYF
-             //out.println(" Barcode   :  "+model[0]+"\n NUXREFSN :  "+model[1]+"\n NUSERIAL : "+model[2]+"\n DTISSUE  :    "+model[3]+"\n CDLOCATTO  :    "+model[4]+"\n CDLOCTYPETO :    "+model[5]+"\n CDCATEGORY  :    "+model[6]+"\n DECOMMODITYF  :    "+model[7]);
+                out.println("Does not exist in system");
+            }
+            else {
+                String model[] = details.split("\\|");
+                System.out.println("DETAILS:"+details+" MODEL LENGTH:"+model.length);
+                // out.println(" Model   :  "+model[0]+"\n Location :  "+model[1]+"\n Manufacturer : "+model[2]+"\n Signed By  :    "+model[3]);
+                //V_NUSENATE,V_NUXREFSN,V_NUSERIAL,V_DTISSUE,V_CDLOCATTO,V_CDLOCTYPETO,V_CDCATEGORY,V_DECOMMODITYF
+                //out.println(" Barcode   :  "+model[0]+"\n NUXREFSN :  "+model[1]+"\n NUSERIAL : "+model[2]+"\n DTISSUE  :    "+model[3]+"\n CDLOCATTO  :    "+model[4]+"\n CDLOCTYPETO :    "+model[5]+"\n CDCATEGORY  :    "+model[6]+"\n DECOMMODITYF  :    "+model[7]);
 
-            //Psuedo JSON for now
-             out.println("{\"nusenate\":\""+model[0]+"\",\"nuxrefsn\":\""+model[1]+"\",\"dtissue\":\""+model[3]+"\",\"cdlocatto\":\""+model[4]+"\",\"cdloctypeto\":\""+model[5]+"\",\"cdcategory\":\""+model[6]+"\",\"adstreet1to\":\""+model[7].replaceAll("\"", "&#34;") +"\",\"decommodityf\":\""+model[8].replaceAll("\"", "&#34;")+"\"}");
-    
-          }
-            
-            
-          
-            
-            
-        } finally {            
+                //Psuedo JSON for now
+                out.println("{\"nusenate\":\""+model[0]+"\",\"nuxrefsn\":\""+model[1]+"\",\"dtissue\":\""+model[3]+"\",\"cdlocatto\":\""+model[4]+"\",\"cdloctypeto\":\""+model[5]+"\",\"cdcategory\":\""+model[6]+"\",\"adstreet1to\":\""+model[7].replaceAll("\"", "&#34;") +"\",\"decommodityf\":\""+model[8].replaceAll("\"", "&#34;")+"\"}");
+            }
+        }
+        finally {            
             out.close();
         }
-    }
+    } // processRequest()
+
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -91,9 +84,12 @@ public class Search extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+                   throws ServletException, IOException
+    {
         processRequest(request, response);
-    }
+    } // doGet()
+
+
 
     /**
      * Handles the HTTP
@@ -106,9 +102,12 @@ public class Search extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+                   throws ServletException, IOException
+    {
         processRequest(request, response);
-    }
+    } // doPost()
+
+
 
     /**
      * Returns a short description of the servlet.
@@ -116,7 +115,8 @@ public class Search extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo() {
+    public String getServletInfo()
+    {
         return "Short description";
     }// </editor-fold>
 }
