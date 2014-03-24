e4876662d122ac9c165c6aa850b5995605d0e511
Kevin Caseiras <kcaseiras@gmail.com>
2014-03-24 11:28:23 -0400

    Add server, ip, and username info into log4j configuration
    refs #7610

---
+++ src/main/java/gov/nysenate/inventory/server/LogFilter.java
@@ -0,0 +1,62 @@
+package gov.nysenate.inventory.server;
+
+import org.apache.log4j.MDC;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = { "/*" })
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        putUserName(req);
+        putServerName();
+        putClientIpAddress(req);
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove("userName");
+            MDC.remove("server");
+            MDC.remove("ipAddress");
+        }
+    }
+
+    private void putUserName(HttpServletRequest req) {
+        HttpSession session = req.getSession(true);
+        String username = null;
+        if (session != null) {
+            username = (String) session.getAttribute("user");
+        }
+        if (username != null) {
+            MDC.put("userName", username);
+        }
+    }
+
+    private void putServerName() {
+        String server = new DbConnect().getDatabaseName();
+        String[] array = server.split(":");
+        MDC.put("server", array[array.length - 1]);
+    }
+
+    private void putClientIpAddress(HttpServletRequest req) {
+        String ipAddress = req.getRemoteAddr();
+        MDC.put("ipAddress", ipAddress);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
