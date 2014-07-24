d7a06f8aac5722959843c9ea40d6aaf3e22032d8
Brian Heitner <heitner@nysenate.gov>
2014-07-24 15:52:36 -0400

    Fix issue with Filter causing downloads sizes of zero. refs #8002

--- src/main/java/gov/nysenate/inventory/server/SessionFilter.java
+++ src/main/java/gov/nysenate/inventory/server/SessionFilter.java
@@ -23,47 +23,63 @@ import org.apache.log4j.MDC;
 /**
  *
  * @author Brian Heitner
+ * 
+ * SessionFilter looks for a Session and returns "Session timed out" if one is not found before
+ * any request to any servlet other than the Servlets that are marked with allowRequest = true.
+ * 
  */
-@WebFilter(urlPatterns = { "/*" })
+@WebFilter(urlPatterns = {"/*"})
 public class SessionFilter implements Filter {
 
     private static final Logger log = Logger.getLogger(SessionFilter.class.getName());
 
     public void doFilter(ServletRequest req, ServletResponse res,
             FilterChain chain) throws IOException, ServletException {
- 
+
+        log.debug("(SessionFilter start) testing");
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         String url = request.getServletPath();
         boolean allowedRequest = false;
         log.debug("(SessionFilter)");
-        
-        PrintWriter out = response.getWriter();
+
         String path = request.getRequestURI();
-        log.debug("(SessionFilter) Path:"+path);
-        
+        log.debug("(SessionFilter) Path:" + path);
+
         /*
-         * Servlets/URLs where there would be no Sessions will set allowRequest = true so this filter will
+         * Servlets/URLs where there should be no Sessions will set "allowRequest = true" so this filter will
          * not attempt to circumvent the request if no Session is found.
+         * 
+         * In the future, a property may be useful in determining the paths that should be allowed through
+         * without checking for a session but for the near future, the following four servlets should be the only
+         * ones.
+         * 
          */
-        
-        if(path.equals("/InventoryWebApp/")|| path.startsWith("/InventoryWebApp/Login")||path.equals("/InventoryWebApp/CheckAppVersion")||path.equals("/InventoryWebApp/DownloadServlet")) {
+
+        if (path.equals("/InventoryWebApp/") || path.startsWith("/InventoryWebApp/Login") || path.equals("/InventoryWebApp/CheckAppVersion") || path.equals("/InventoryWebApp/DownloadServlet")) {
             allowedRequest = true;
         }
-             
+
         if (!allowedRequest) {
             HttpSession session = request.getSession(false);
             if (null == session) {
-                  log.debug("(SessionFilter)Session timed out");
-                  out.println("Session timed out");
-                  return;
+                log.debug("(SessionFilter) Right before opening output stream for Session time out. "+path);
+                PrintWriter out = response.getWriter();
+                log.debug("(SessionFilter)Session timed out");
+                try {
+                    out.flush();
+                    out.close();
+                } catch (Exception e) {
+                    log.error(null, e);
+                }
+                return;
             }
         }
-         
-        chain.doFilter(req, res);
-    }    
 
-    public void destroy() {
+        chain.doFilter(req, res);
+    }
+
+    public void destroy() {        
         /*
          * called before the Filter instance is removed from service by the web
          * container
