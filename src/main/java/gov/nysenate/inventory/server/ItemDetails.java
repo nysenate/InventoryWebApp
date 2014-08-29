b77c752b55c543c3ae520262875fcc28b64a5992
Kevin Caseiras <kcaseiras@gmail.com>
2014-08-29 13:01:15 -0400

    Check removal status when getting item info.
    refs #8057

--- src/main/java/gov/nysenate/inventory/server/ItemDetails.java
+++ src/main/java/gov/nysenate/inventory/server/ItemDetails.java
@@ -39,7 +39,7 @@ public class ItemDetails extends HttpServlet {
                 String model[] = details.split("\\|");
 
                 //Psuedo JSON for now
-                out.println("{\"nusenate\":\"" + model[0] + "\",\"nuxrefsn\":\"" + model[1] + "\",\"dtissue\":\"" + model[3] + "\",\"cdlocatto\":\"" + model[4] + "\",\"cdloctypeto\":\"" + model[5] + "\",\"cdcategory\":\"" + model[6] + "\",\"adstreet1to\":\"" + model[7].replaceAll("\"", "&#34;") + "\",\"decommodityf\":\"" + model[8].replaceAll("\"", "&#34;")  + "\",\"cdlocatfrom\":\"" + model[9] + "\",\"cdstatus\":\"" + model[10] + "\",\"cdintransit\":\"" + model[12] + "\"}");
+                out.println("{\"nusenate\":\"" + model[0] + "\",\"nuxrefsn\":\"" + model[1] + "\",\"dtissue\":\"" + model[3] + "\",\"cdlocatto\":\"" + model[4] + "\",\"cdloctypeto\":\"" + model[5] + "\",\"cdcategory\":\"" + model[6] + "\",\"adstreet1to\":\"" + model[7].replaceAll("\"", "&#34;") + "\",\"decommodityf\":\"" + model[8].replaceAll("\"", "&#34;")  + "\",\"cdlocatfrom\":\"" + model[9] + "\",\"cdstatus\":\"" + model[10] + "\",\"cdintransit\":\"" + model[12] + "\",\"pending_removal\":\"" + model[14] + "\"}");
             }
 
         } finally {
