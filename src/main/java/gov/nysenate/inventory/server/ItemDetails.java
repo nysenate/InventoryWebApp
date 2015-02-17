a0ef175d7293cc6016c75af10daa9b61ea081a0c
Kevin Caseiras <kcaseiras@gmail.com>
2015-02-17 16:15:46 -0500

    Refactor out manual json in ItemDetails servlet

--- src/main/java/gov/nysenate/inventory/server/ItemDetails.java
+++ src/main/java/gov/nysenate/inventory/server/ItemDetails.java
@@ -1,17 +1,21 @@
 package gov.nysenate.inventory.server;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import gov.nysenate.inventory.dao.DbConnect;
+import gov.nysenate.inventory.dao.item.ItemService;
+import gov.nysenate.inventory.model.Item;
+import gov.nysenate.inventory.util.HttpUtils;
+import gov.nysenate.inventory.util.Serializer;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import gov.nysenate.inventory.dao.DbConnect;
-import gov.nysenate.inventory.util.HttpUtils;
-import org.apache.log4j.Logger;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 /**
  *
@@ -20,34 +24,30 @@ import org.apache.log4j.Logger;
 @WebServlet(name = "ItemDetails", urlPatterns = {"/ItemDetails"})
 public class ItemDetails extends HttpServlet {
 
-    private static final Logger log = Logger.getLogger(ItemDetails.class.getName());
+    private static final Logger log = Logger.getLogger(ItemDetails.class);
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json");
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession(false);
         DbConnect db = new DbConnect(HttpUtils.getUserName(session), HttpUtils.getPassword(session));
 
+        String barcode = request.getParameter("barcode_num");
+        log.info("Searching for item with barcode: " + barcode);
+
+        Item item = retrieveItem(db, barcode);
+        out.write(Serializer.serialize(item));
+    }
+
+    private Item retrieveItem(DbConnect db, String barcode) {
+        Item item = null;
         try {
-            String barcode_num = request.getParameter("barcode_num");
-            log.info("Getting item info for barcode = " + barcode_num);
-
-            String details = db.getDetails(barcode_num);
-
-            log.info("Item details for " + barcode_num + " = " + details);
-            if (details.equals("no")) {
-                out.println("Does not exist in system");
-            } else {
-                String model[] = details.split("\\|");
-
-                //Psuedo JSON for now
-                out.println("{\"nusenate\":\"" + model[0] + "\",\"nuxrefsn\":\"" + model[1] + "\",\"dtissue\":\"" + model[3] + "\",\"cdlocatto\":\"" + model[4] + "\",\"cdloctypeto\":\"" + model[5] + "\",\"cdcategory\":\"" + model[6] + "\",\"adstreet1to\":\"" + model[7].replaceAll("\"", "&#34;") + "\",\"decommodityf\":\"" + model[8].replaceAll("\"", "&#34;")  + "\",\"cdlocatfrom\":\"" + model[9] + "\",\"cdstatus\":\"" + model[10] + "\",\"cdintransit\":\"" + model[12] + "\",\"pending_removal\":\"" + model[14] + "\"}");
-            }
-
-        } finally {
-            out.close();
+            item = new ItemService().getItemByBarcode(db, barcode);
+        } catch (SQLException | ClassNotFoundException ex) {
+            log.error("Error searching for item.", ex);
         }
+        return item;
     }
 
     @Override
