0297bdbbba2371f6e898157e68022c5041a93afd
Kevin Caseiras <kcaseiras@gmail.com>
2015-03-31 11:16:47 -0400

    Automatically save each server log to its own file

---
+++ src/main/java/gov/nysenate/inventory/listener/LogConfiguration.java
@@ -0,0 +1,38 @@
+package gov.nysenate.inventory.listener;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class LogConfiguration implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Properties appProperties = loadProperties("config.properties");
+        String server = appProperties.getProperty("connectionString");
+        String filename = "InventoryWebApp_" + server.split("/")[1] + ".log";
+        Properties logProperties = loadProperties("log4j.properties");
+        logProperties.put("log.filename", filename);
+        PropertyConfigurator.configure(logProperties);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+
+    private Properties loadProperties(String propertyFile) {
+        Properties properties = new Properties();
+        InputStream in = getClass().getClassLoader().getResourceAsStream(propertyFile);
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
